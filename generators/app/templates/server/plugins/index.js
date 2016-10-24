57d2d4f3fd6d1a6e97bf3c9045d81407aa297d13
duivvv <geoffrey.dhuyvetters@gmail.com>
2016-10-24 15:13:01 +0200

    changes in plugin index.js, loads folders index.js now

--- generators/app/templates/server/plugins/index.js
+++ generators/app/templates/server/plugins/index.js
@@ -1,4 +1,5 @@
 const fs = require(`fs`);
+const path = require(`path`);
 
 const pluginHandler = require(`../lib/pluginHandler`);
 const isValidName = require(`../lib/isValidName`);
@@ -6,6 +7,12 @@ const isValidName = require(`../lib/isValidName`);
 module.exports.register = (server, options, next) => {
 
   fs.readdirSync(__dirname).forEach(f => {
+
+    /** if DIR, load index.js **/
+    const stat = fs.lstatSync(path.join(__dirname, f));
+    if(stat.isDirectory() && !f.startsWith(`_`)){
+      server.register(require(path.join(__dirname, f)), pluginHandler);
+    }
 
     if(!isValidName(f)) return;
     server.register(require(`./${f}`), pluginHandler);
