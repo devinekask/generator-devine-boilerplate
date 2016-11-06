4fce42820a8d4d53deb786cdae0bdc64f8d06a24
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-11-06 22:36:33 +0100

    try/catch check for yarn

--- generators/app/index.js
+++ generators/app/index.js
@@ -60,8 +60,11 @@ module.exports = generator.Base.extend({
 
     };
 
-    /* yarn check sync (try, catch) */
-
+    try {
+      require(`child_process`).execSync(`yarn --version >/dev/null 2>&1`, {encoding: `utf8`});
+    } catch (e) {
+      this.props.yarn = false;
+    }
   },
 
   prompting(){
