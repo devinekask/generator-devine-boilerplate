e30beacf3450c5d18358e37b7e81687d1d9f8f5d
duivvv <geoffrey.dhuyvetters@gmail.com>
2017-04-28 20:28:55 +0200

    :bookmark: Release 3.16.1

--- generators/app/templates/server/schemas/User.js
+++ generators/app/templates/server/schemas/User.js
@@ -1,46 +1,40 @@
-const Schema = require(`mongoose`).Schema;
-
 const Scopes = require(`../const/Scopes`);
+const Joi = require(`joi`);
 
-const schema = new Schema({
+const schema = {
 
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    validation: Joi.string().alphanum().min(3),
   },
 
   password: {
     type: String,
     required: true,
-    bcrypt: true
+    validation: Joi.string().min(3),
+    project: false
   },
 
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    validation: Joi.string().email()
   },
 
   scope: {
     type: String,
-    default: Scopes.USER
-  },
-
-  isActive: {
-    type: Boolean,
-    default: true
+    default: Scopes.USER,
+    validation: Joi.string().min(3)
   }
 
-}, {
+};
 
-  timestamps: {
-    createdAt: `created`,
-    updatedAt: `modified`
-  }
-
-});
-
-schema.plugin(require(`mongoose-bcrypt`));
-
-module.exports = {schema};
+module.exports = {
+  schema,
+  plugins: [
+    require(`mongoose-bcrypt`)
+  ]
+};
